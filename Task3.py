5ccd709b4080a83eb1d435b9e817bde440305d8a
Екатерина <katusha_orlova@mail.ru>
2022-07-04 22:44:38 +0700

    Задание 3

---
+++ Task3.py
@@ -0,0 +1,8 @@
+# Создание функции, которая принимает два числа и строку
+def vyvod(a, b, c):
+    print(a, b, c)
+a = int(input())
+b = int(input())
+c = str(input())
+print (vyvod(a, b, c))
+print (vyvod(3, 7, "*"))
